9c04cef8680345dbb01eaa29691ad3bb56a246f3
chelsea-banke <bankechelsea@gmail.com>
2021-07-15 02:09:42 +0100

    simple pages_app creation
    The pages folder contains the project in which the pages_app and Template directory have been created and stored
    
    the Template directory contains the two html files which are the home and about pages but also has a base html file which serves in carrying inheritable properties of the web pages

---
+++ pages/pages_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'Home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'About.html'
+
